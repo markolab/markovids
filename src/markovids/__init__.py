5cb6c1e6596e492a88786a03bf049d93efcf6781
Jeffrey Markowitz <jeffrey.markowitz@bme.gatech.edu>
2023-06-25 04:25:02 +0000

    Added file reader class

--- src/markovids/__init__.py
+++ src/markovids/__init__.py
@@ -1,7 +1,7 @@
 import markovids.pcl.io
 import markovids.pcl.registration
 import markovids.vid.io
-import markovids.vid.io.RawFileReader
+from markovids.vid.io import RawFileReader
 import markovids.depth.plane
 import markovids.depth.filter
 import markovids.depth.moments
