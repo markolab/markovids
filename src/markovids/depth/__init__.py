26bab75b3ec3f2fec2b7d18a417b1b58fdd7978c
Jeffrey Markowitz <jeffrey.markowitz@bme.gatech.edu>
2024-12-18 12:20:48 -0500

    Updated init to speedup initialization

---
+++ src/markovids/depth/__init__.py
@@ -0,0 +1,6 @@
+# from markovids.vid.io import RawFileReader
+import markovids.depth.plane
+import markovids.depth.filter
+import markovids.depth.moments
+import markovids.depth.track
+import markovids.depth.io
---
+++ src/markovids/pcl/__init__.py
@@ -0,0 +1,6 @@
+# note that these are optional...
+try:
+	import markovids.pcl.io
+	import markovids.pcl.registration
+except ModuleNotFoundError:
+	pass
