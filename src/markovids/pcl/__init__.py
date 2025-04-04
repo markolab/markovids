fdd380399471dea2aa5fb63cddd4af08bd23d46c
Jeffrey Markowitz <jeffrey.markowitz@bme.gatech.edu>
2025-04-04 16:14:45 -0400

    Cleaned up imports

--- src/markovids/pcl/__init__.py
+++ src/markovids/pcl/__init__.py
@@ -1,6 +1,11 @@
 # note that these are optional...
-import markovids.pcl.io
-import markovids.pcl.registration
-import markovids.pcl.kpoints
-import markovids.pcl.viz
-import markovids.pcl.pipeline
+from . import io
+from . import registration
+from . import kpoints
+from . import viz
+from . import pipeline
+# import markovids.pcl.io
+# import markovids.pcl.registration
+# import markovids.pcl.kpoints
+# import markovids.pcl.viz
+# import markovids.pcl.pipeline
